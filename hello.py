678721d4de529aa97a7491a7e7e8bb9487c26c24
George Hernandez <georgelhernandez@gmail.com>
2022-09-30 17:20:55 -0500

    Notes on venv

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
 # Run me with: python hello.py
+# If VSC & Python extension installed, then run by right-click to "Run Python File in Terminal"
 who = input()
-print('Hello', who)
+    print('Hello', who)
+my_dict = {
+    'user': 'Marco', "addrss": '123 Main St',
+            'age': 25
+}
+
+
