deeefaf5082e912b26a955110199596c7c1019f9
George Hernandez <georgelhernandez@gmail.com>
2022-10-03 12:40:40 -0500

    Tidy

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
-"""This is a Hello world test"""
+"""
+This is a Hello world test.
+Every module should have a module-level docstring.
+"""
+
 # Run me with: python hello.py
 # If VSC & Python extension installed,
 # then run by right-click to "Run Python File in Terminal"
@@ -12,7 +16,7 @@ my_dict = {"user": "George", "address": "123 Main St", "age": 25}
 
 # Variant with unlabled input request:
 def hello(name):
-    """Take a name and respond"""
+    """Take a name and respond."""
     return "Hello " + name
 
 
