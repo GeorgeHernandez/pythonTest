b5dfd80bcc82683427ddf6e1a7ef2f6ddca051a7
George Hernandez <georgelhernandez@gmail.com>
2022-10-03 11:13:03 -0500

    Tweaks to test linting & formatting

--- hello.py
+++ hello.py
@@ -1,10 +1,15 @@
 # Run me with: python hello.py
-# If VSC & Python extension installed, then run by right-click to "Run Python File in Terminal"
+# If VSC & Python extension installed,
+# then run by right-click to "Run Python File in Terminal"
 who = input()
-    print('Hello', who)
+    print('Hello', who) # Purposeful unexpected indentation
 my_dict = {
-    'user': 'Marco', "addrss": '123 Main St',
+    'user': 'George', "address": '123 Main St',
             'age': 25
 }
 
+def hello (name):
+    return"Hello"+name
+name = input("Enter your name: ")
+print(hello(name ))
 
